802ef7275ccbc04b7195ed2807de84d84d060df6
Matěj Polák <matpolak@seznam.cz>
2017-09-29 09:56:13 +0200

    gategory. php + gender.php + db.php + includes + bootstrap linked into the index.php

---
+++ category.php
@@ -0,0 +1,7 @@
+<?php
+$category = [
+    0 => 'Sport',
+    1 => 'Luxury',
+    2 => 'Kids',
+    3 => 'Smart',
+];
---
+++ gender.php
@@ -0,0 +1,6 @@
+<?php
+$gender = [
+    0 => 'Male',
+    1 => 'Female',
+    2 => 'Unisex',
+];
