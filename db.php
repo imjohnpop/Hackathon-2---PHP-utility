037acef0bdb88a99430ae6fd9e309d092b36b02b
imjohnpop <poprocsi.j@gmail.com>
2017-09-29 10:02:02 +0200

    changed the database name in the db.php to products

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 function db_connect() {
-    $db = new PDO('mysql:host=localhost;dbname=movie_base;charset=utf8', 'root', 'rootroot');
+    $db = new PDO('mysql:host=localhost;dbname=products;charset=utf8', 'root', 'rootroot');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
